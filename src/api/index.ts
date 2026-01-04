e33cb5277c54c5775f9fe18c7a258f66cc116759
Lucas Mercado <lmercado@sisu.mx>
2026-01-04 17:25:55 -0300

    initial socket working

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,11 +5,14 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server({
+  cors: { origin: "http://localhost:5173" }
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 });
+io.listen(server);
 
 server.listen(4000, () => {
   console.log("server is running on port 4000");
